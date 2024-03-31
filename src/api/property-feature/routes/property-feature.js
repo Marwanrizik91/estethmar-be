715ed8fbfd9e8adf4ef1387aa15688ab7d061136
Marwan Rizik <marwan.rizik@gmail.com>
2024-03-31 12:30:07 +0300

    project/property features

---
+++ src/api/property-feature/routes/property-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * property-feature router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::property-feature.property-feature');
---
+++ src/api/property-feature/services/property-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * property-feature service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::property-feature.property-feature');
